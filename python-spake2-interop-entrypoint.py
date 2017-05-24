d77f8f152ac905f4446aef64fac9498041fd19b5
Jonathan Lange <jml@mumak.net>
2017-05-24 09:25:14 +0100

    Allow params to be specified to Python entry point

--- python-spake2-interop-entrypoint.py
+++ python-spake2-interop-entrypoint.py
@@ -4,6 +4,8 @@ from __future__ import print_function, unicode_literals
 
 from sys import argv, stdout
 
+from spake2 import params
+
 if argv[1] == b"A":
     from spake2 import SPAKE2_A as SPAKE2_SIDE
 elif argv[1] == b"B":
@@ -15,7 +17,24 @@ else:
 
 password = argv[2]
 
-s = SPAKE2_SIDE(password)
+PARAMS = {
+    'I1024': params.Params1024,
+    'I2048': params.Params2048,
+    'I3072': params.Params3072,
+    'Ed25519': params.ParamsEd25519,
+}
+
+param = params.ParamsEd25519
+if len(argv) > 3:
+    try:
+        params = PARAMS[argv[3]]
+    except ValueError:
+        raise ValueError(
+            'Choose a valid group to use (one of %r), got %s'
+            % (list(PARAMS.keys()), param))
+
+
+s = SPAKE2_SIDE(password, params=param)
 msg_out = s.start()
 print(msg_out.encode("hex"))
 stdout.flush()
